c8cfcf4b231ed6cca162e434e8c5ed9b3f94d594
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-03-12 19:48:24 -0500

    Inicializaciión base del AR venta

--- src/main/java/ec/com/proeventos/domain/ventas/Venta.java
+++ src/main/java/ec/com/proeventos/domain/ventas/Venta.java
@@ -1,5 +1,17 @@
 package ec.com.proeventos.domain.ventas;
 
-public class Venta {
-    
+import co.com.sofka.domain.generic.AggregateEvent;
+import ec.com.proeventos.domain.ventas.valor.Fecha;
+import ec.com.proeventos.domain.ventas.valor.VentaId;
+
+public class Venta extends AggregateEvent<VentaId>{
+    protected Cliente cliente;
+    protected Vendedor vendedor;
+    protected Contrato contrato;
+    protected Fecha fecha;
+
+    public Venta (VentaId ventaId, Vendedor vendedor){
+        super(ventaId);
+    }
+
 }
