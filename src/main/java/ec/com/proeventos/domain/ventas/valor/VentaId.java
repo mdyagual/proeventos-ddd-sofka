cf96defabb301d3e455fa51d6cc6b6528eef63d6
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-03-12 19:47:18 -0500

    Creación de clase Identity ventaID para Venta

---
+++ src/main/java/ec/com/proeventos/domain/ventas/valor/VentaId.java
@@ -0,0 +1,17 @@
+package ec.com.proeventos.domain.ventas.valor;
+
+import co.com.sofka.domain.generic.Identity;
+
+public class VentaId extends Identity{
+    private VentaId(String valor){
+        super(valor);
+    }
+
+    public VentaId(){
+
+    }
+
+    public static VentaId of (String valor){
+        return new VentaId(valor);
+    }
+}
