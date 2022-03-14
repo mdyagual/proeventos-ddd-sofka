73989f1edb8da7627a9c19c0cd0cc77875c037bf
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-03-14 04:34:02 -0500

    Implmenetacion de prueba para crearAnimador en el dominio de evento - Prueba pasada

--- src/test/java/ec/com/proeventos/domain/evento/usecases/CrearAnimadorUseCaseTest.java
+++ src/test/java/ec/com/proeventos/domain/evento/usecases/CrearAnimadorUseCaseTest.java
@@ -1,5 +1,87 @@
 package ec.com.proeventos.domain.evento.usecases;
 
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import co.com.sofka.business.generic.UseCaseHandler;
+import co.com.sofka.business.repository.DomainEventRepository;
+import co.com.sofka.business.support.RequestCommand;
+import co.com.sofka.domain.generic.DomainEvent;
+import ec.com.proeventos.domain.evento.valor.AnimadorId;
+import ec.com.proeventos.domain.evento.valor.CelebracionId;
+import ec.com.proeventos.domain.evento.valor.Detalle;
+import ec.com.proeventos.domain.evento.valor.Duracion;
+import ec.com.proeventos.domain.evento.valor.EventoId;
+import ec.com.proeventos.domain.evento.valor.Lugar;
+import ec.com.proeventos.domain.planificador.valor.PlanificadorId;
+import ec.com.proeventos.domain.evento.Animador;
+import ec.com.proeventos.domain.evento.Celebracion;
+import ec.com.proeventos.domain.evento.command.CrearAnimador;
+import ec.com.proeventos.domain.evento.event.AnimadorCreado;
+import ec.com.proeventos.domain.evento.event.EventoCreado;
+
+@ExtendWith(MockitoExtension.class)
 public class CrearAnimadorUseCaseTest {
+    @Mock
+    private DomainEventRepository repository;
+
+    @Test
+    void CrearAnimador(){
+        //Arange
+        EventoId eventoId = EventoId.of("evento1");
+
+        AnimadorId animadorId=AnimadorId.of("animador1");
+        Detalle detalle = new Detalle("NombreX", "ApellidoX", "M");
+
+        var command = new CrearAnimador(eventoId,animadorId,detalle);
+        
+        var useCase = new CrearAnimadorUseCase();
+
+        Mockito.when(repository.getEventsBy("evento1")).thenReturn(history());
+        useCase.addRepository(repository);
+
+        //Act  
+        var events = UseCaseHandler.getInstance()
+        .setIdentifyExecutor(eventoId.value())
+        .syncExecutor(useCase, new RequestCommand<>(command))
+        .orElseThrow()
+        .getDomainEvents();
+
+        //Asset
+        var event = (AnimadorCreado) events.get(0);
+        Assertions.assertEquals("evento.animadorcreado", event.type);
+        Assertions.assertEquals("evento1", event.aggregateRootId());
+        Assertions.assertEquals(animadorId, event.getAnimadorId());
+        Assertions.assertEquals(detalle, event.getDetalle());   
+
+
+    }
+
+    private List<DomainEvent> history() {        
+        //id,Celebracion,Animador,PlanificadorId
+        CelebracionId celebracionId = CelebracionId.of("celebracion1");
+        Lugar lugar = new Lugar("LugarX", "DireccionX", 500);
+        Duracion duracion = new Duracion("8H00", "10H00");
+        Celebracion celebracion = new Celebracion(celebracionId, lugar, duracion);
+
+        AnimadorId animadorId=AnimadorId.of("animador1");
+        Detalle detalle = new Detalle("NombreX", "ApellidoX", "M");
+        Animador animador = new Animador(animadorId, detalle);
+
+        PlanificadorId planficadorId = PlanificadorId.of("planificador1");
+
+        return List.of(
+                    new EventoCreado(planficadorId, celebracion, animador),
+                    new AnimadorCreado(animadorId,detalle)
+            );
+        
+        }
     
 }
