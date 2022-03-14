b082162c2ec1360021a9b8401053d0133697a404
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-03-14 03:32:47 -0500

    Inicialización de casos de prueba para los UseCase Âactivos en Planificador y Evento

---
+++ src/test/java/ec/com/proeventos/domain/evento/usecases/CrearAnimadorUseCaseTest.java
@@ -0,0 +1,5 @@
+package ec.com.proeventos.domain.evento.usecases;
+
+public class CrearAnimadorUseCaseTest {
+    
+}
---
+++ src/test/java/ec/com/proeventos/domain/planificador/usecases/CrearRegistroUseCaseTest.java
@@ -0,0 +1,5 @@
+package ec.com.proeventos.domain.planificador.usecases;
+
+public class CrearRegistroUseCaseTest {
+    
+}
